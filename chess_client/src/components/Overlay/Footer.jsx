924c471874859a520ee510e69ef1521cb962a3c7
Aditya Sharma <adityas1709@gmail.com>
2025-03-25 18:55:14 +0530

    Maybe footer fixed?

--- chess_client/src/components/Overlay/Footer.jsx
+++ chess_client/src/components/Overlay/Footer.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-export default function Footer(){
-    return(
-        <footer className="bg-slate-950 py-10">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row justify-between items-center">
-            <div className="text-2xl font-bold mb-4 md:mb-0">
-              MY<span className="text-amber-700">CHESS</span>
-            </div>
-            <div className="flex justify-center">
-            <div className="text-sm text-gray-400 ">
-                © {new Date().getFullYear()} MyChess by PDA
-                <br/>
-                Made with love by Adidev, Aditya, and Prateek
-            </div>
-            </div>
-           
+export default function Footer() {
+  return (
+    <footer className="bg-slate-950 py-10">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="text-2xl font-bold mb-4 md:mb-0">
+            MY<span className="text-amber-700">CHESS</span>
+          </div>
+          {}
+          <div className="flex-1"></div>
+          <div className="text-sm text-gray-400 text-center">
+            © {new Date().getFullYear()} MyChess by PDA
+            <br />
+            Made with love by Adidev, Aditya, and Prateek
           </div>
         </div>
-      </footer>
-    )
+      </div>
+    </footer>
+  );
 }
