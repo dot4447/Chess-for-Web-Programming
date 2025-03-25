aa7cef4233c468db6e166700052f3272325a99d6
prateekpurohit13 <prateekpurohit171@gmail.com>
2025-03-25 15:04:25 +0530

    Added multiplayer mode and organized folder structure

---
+++ chess_client/src/components/index.js
@@ -0,0 +1,7 @@
+import Layout from "../Layout";
+import Home from "./home/Home";
+import Offline_practice from "./Offline/Offline";
+import Play from "./Online/src/Play"
+export default {Layout}
+
+export {Layout, Home, Offline_practice, Play}
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Layout from "../Layout";
-import Home from "./home/Home";
-import Online from "./Online/Online";
-import Offline_practice from "./Offline/Offline";
-export default {Layout}
-
-export {Layout, Home, Online, Offline_practice}
